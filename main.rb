ffb4209c4111f2415605640f5fe8e598445c69e8
Ash <161075247+zararashigal@users.noreply.github.com>
2024-06-04 13:56:11 +0200

    Update test dictionary, Commentary

--- main.rb
+++ main.rb
@@ -1,31 +1,48 @@
+# Initialize Array
 $palindromes = Array.new()
 
+# Palindrome checker uwu
 def isPalindrome(str)
 
-    str.downcase!
-    str.gsub!(/[\W_]/, "")
+    # Downcase, remove spaceesss...
+    checkstr = str.downcase
+    checkstr.gsub!(/[\W_]/, "")
 
-    if str.reverse == str && str.length > 2 then $palindromes.push(str) end
+    # If the reverse of this string is the same, it is a palindrome!
+    if checkstr.reverse == checkstr && checkstr.length > 2 then $palindromes.push(str) end
 
 end
 
+# Scan (file)
 def scan (name)
 
+    # Statistics...
     $statsStart = Time.now()
     $statsLines = 0
+
+    # Start scanning the file line by line!
     File.readlines(name, chomp: true).each do |l| 
+        
+        # Add to statistic linecount,
         $statsLines+=1 
+
+        # Is it a palindrome? Add to the palindrome array!
         isPalindrome(l) 
+
     end
 
-    puts "\nPalindromes in #{ARGV[0]} [#{$palindromes.count()}]:\n#{$palindromes.join(", ")}"
+    # Output informationnss
+    puts "\nPalindromes in #{ARGV[0]} [#{$palindromes.count}]:\n#{$palindromes.join(", ")}"
     
+    # Statistics...
     $statsEnd = Time.now()
     $statsTot = $statsEnd - $statsStart
 
-    puts "\nLaladome processed #{$statsLines} lines in #{$statsTot} seconds!\n\n"
+    # Statistics 2 de statening! xD
+    puts "\nLaladome processed #{$statsLines} lines in #{$statsTot} seconds and has found #{$palindromes.count} palindromes in the file!\n\n"
 
 end
 
+# Start scanning!
 puts "\nLaladome (c) 2024 Zarashigal\nScanning for palindromes in #{ARGV[0]}..."
 scan ARGV[0]
